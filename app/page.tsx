14e395f0845ef7d1faf22e693287013b43aea114
kartikp36 <kartikp2002@gmail.com>
2024-11-21 00:01:11 +0530

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+	return (
+		<div className="flex h-full min-h-screen w-full items-center justify-center">
+			<Button>Click!</Button>
+		</div>
+	)
+}
