b52825efdfa214cf380738c5302541ae69d1a517
Kartik P. Patel <36930635+kartikp36@users.noreply.github.com>
2024-11-25 10:55:10 +0530

    fix: export font build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
 	subsets: ['latin'],
 	display: 'swap',
 	variable: '--font-sans',
